2575e662de15beb4e00772db4ba04a1b157f6c3f
inh556 <yanbin.jin@gmail.com>
2018-04-12 19:48:06 -0700

    finished staic part

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -9,7 +9,33 @@ constructor(props) {
   }
   render() {
     return (
-      <div>App says hello</div>
+      <div id="header">
+        <div className="header-left">
+          <div className="titleClass">
+            <div className="titleLine" id="restaurantName">Bunn Mike</div>
+            <div className="titleLine" id="restaurantClaimed">claimed</div>
+            <div className="titleLine" id="restaurantClaimed">claimed</div>
+          </div>
+          <div className="ratingClass">
+            <div className="reviewLine" id="restaurantStars">stars</div>
+            <div className="reviewLine" id="restaurantReviewCount">16 reviews</div>
+            <button className="reviewLine" id="restaurantDetail">Detail</button>
+          </div>
+          <div className="categoryClass">
+      
+            <a className="categories" href="https://www.w3schools.com">Mexican,</a>
+            <a className="categories" href="https://www.w3schools.com">Taco,</a>
+            <a className="categories" href="https://www.w3schools.com">Lunch</a>
+            <button className="categories" id="restaurantEdit">edit</button>
+          </div>
+        </div>
+        <div className="actionClass">
+          <button className="actionElements" id="writeReview">Write A Review</button>
+          <button className="actionElements" id="addPhoto">Add Photos</button>
+          <button className="actionElements" id="sharing">Sharing</button>
+          <button className="actionElements" id="addToBookMark">Book Mark</button>
+        </div>
+      </div>
     );
   }
 }
