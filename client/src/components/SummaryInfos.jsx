e6eecb63da832edeceb939ed9c8e3bdf78603e69
inh556 <yanbin.jin@gmail.com>
2018-04-16 19:59:27 -0700

    add pictures

--- client/src/components/SummaryInfos.jsx
+++ client/src/components/SummaryInfos.jsx
@@ -1,26 +1,27 @@
 import React from 'react';
 
-const SummaryInfos = props => (
-  <div className="header-left">
-    <div className="titleClass">
-      <div className="titleLine" id="restaurantName">{ props.infos.name }</div>
-      <div className="titleLine" id="restaurantClaimed">CheckMarkHere</div>
-      <div className="titleLine" id="restaurantClaimed"><a data-toggle="tooltip" data-placement="top" title="This business has been claimed by the owner or a representative!">claimed</a></div>
-    </div>
-    <div className="ratingClass">
-      <div className="reviewLine" id="restaurantStars">{props.infos.stars}</div>
-      <div className="reviewLine" id="restaurantReviewCount">{ props.infos.reviewCount } reviews</div>
-      <button className="reviewLine" id="restaurantDetail">Detail</button>
-    </div>
-    <div className="categoryClass">
-      <span id="priceRange">$$</span>
-      <span id="bullet">•</span>
-      <a className="categories" href="https://www.yelp.com/search?find_desc=mexican&find_loc=Financial+District,+San+Francisco,+CA">Mexican,</a>
-      <a className="categories" href="https://www.yelp.com/search?find_desc=taco&find_loc=Financial+District,+San+Francisco,+CA">Taco,</a>
-      <a className="categories" href="https://www.yelp.com/search?find_desc=lunch&find_loc=Financial+District,+San+Francisco,+CA">Lunch</a>
-      <button className="categories" id="restaurantEdit">edit</button>
-    </div>
-  </div>
-);
 
+function SummaryInfos(props) {
+  return (
+    <div className="header-left">
+      <div className="titleClass">
+        <span className="titleLine" id="restaurantName">{ props.infos.name }</span>
+        <span className="titleLine" id="restaurantClaimed">{ props.infos.claimed? <span><img src='./claim.png' alt="claimed" />claimed</span> : 'notclaimed' }</span>
+      </div>
+      <div className="ratingClass">
+        <img className="reviewLine" id="restaurantStars" src={`./${props.infos.stars}.png`} alt={props.infos.stars} />
+        <span className="reviewLine" id="restaurantReviewCount">{ props.infos.reviewCount } reviews</span>
+        <button className="reviewLine" id="restaurantDetail">Detail</button>
+      </div>
+      <div className="categoryClass">
+        <span id="priceRange">{props.infos.restaurantsPriceRange2}</span>
+        <span id="bullet">•</span>
+        <a className="categories" href="https://www.yelp.com/search?find_desc=mexican&find_loc=Financial+District,+San+Francisco,+CA">Mexican,</a>
+        <a className="categories" href="https://www.yelp.com/search?find_desc=taco&find_loc=Financial+District,+San+Francisco,+CA">Taco,</a>
+        <a className="categories" href="https://www.yelp.com/search?find_desc=lunch&find_loc=Financial+District,+San+Francisco,+CA">Lunch</a>
+        <button className="categories" id="restaurantEdit">edit</button>
+      </div>
+    </div>
+  );
+}
 export default SummaryInfos;
